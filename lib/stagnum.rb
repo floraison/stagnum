164e567cfcd25148935a2b9ac154a54cee2a86f3
John Mettraux <jmettraux@gmail.com>
2025-03-04 14:57:18 +0900

    Introduce dedicated DoneQueue

--- lib/stagnum.rb
+++ lib/stagnum.rb
@@ -1,8 +1,109 @@
 # frozen_string_literal: true
 
+require 'thread'
 
-module Stagnum
+
+class Stagnum
 
   VERSION = '1.0.0'
+
+  attr_reader :name
+
+  def initialize(name, worker_count)
+
+    @name = name
+    @work_queue = ::Thread::Queue.new
+    @worker_count = worker_count
+
+    @maintenance_mutex = ::Thread::Mutex.new
+    @next_worker_thread_id = -1
+    @worker_threads = []
+  end
+
+  def enqueue(done_queue=Stagnum::DoneQueue.new, data, &block)
+
+    maintain
+
+    done_queue.increment if done_queue.respond_to?(:increment)
+
+    @work_queue << [ done_queue, data, block ]
+
+    done_queue
+  end
+
+  def next_worker_thread_id
+
+    @next_worker_thread_id += 1
+  end
+
+  protected
+
+  def maintain
+
+    @maintenance_mutex.synchronize do
+
+      @worker_threads = @worker_threads.select { |t| t.alive? }
+
+      while @worker_threads.size < @worker_count
+
+        @worker_threads << Stagnum::WorkerThread.new(self, @work_queue)
+      end
+    end
+  end
+
+  class DoneQueue < ::Thread::Queue
+
+    attr_reader :count
+
+    def initialize(items=[])
+
+      super
+
+      @count
+    end
+
+    def increment
+
+      @count += 1
+    end
+
+    # Returns [ successes, failures ]
+    #
+    def pop_all
+
+      @count.times
+        .inject([ [], [] ]) { |a, i|
+          r = self.pop
+          a[r[0] == :success ? 0 : 1] << r
+          a }
+    end
+  end
+
+  class WorkerThread < ::Thread
+
+    def initialize(stagnum, work_queue)
+
+      @stagnum = stagnum
+      @work_queue = work_queue
+
+      self.name = "#{stagnum.name}__#{stagnum.next_thread_id}"
+
+      super do
+
+        loop do
+
+          done_queue, data, block = @work_queue.pop
+
+          block.call(data)
+
+          done_queue << [ :success, data ]
+
+        rescue => err
+
+          done_queue << [ :failure, data, err ]
+        end
+      end
+    end
+  end
 end
 
---
+++ test/core_test.rb
@@ -0,0 +1,11 @@
+
+#
+# Specifying stagnum
+#
+# Tue Mar  4 14:27:37 JST 2025  The Board Room
+#
+
+
+group Stagnum do
+end
+
